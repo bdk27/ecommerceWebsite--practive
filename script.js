cbcd00bfac6159f3f9e64763b427a387fbbfda1f
BDK27 <briansam195@gmail.com>
2022-07-27 17:59:00 +0800

    RWD

---
+++ script.js
@@ -0,0 +1,25 @@
+let bar = document.getElementById('bar');
+let nav = document.getElementById('navbar');
+let close = document.getElementById('close');
+
+bar.onclick = function (){
+    nav.classList.add('active');
+}
+close.onclick = function (){
+    nav.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
